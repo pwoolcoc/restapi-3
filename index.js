4d5032fa7f544466756c590f6e245b7bc868610c
Paul Woolcock <paul@woolcock.us>
2015-08-24 22:54:03 -0400

    Initial commit of the actualy login/logout endpoints. Just the login endpoint right now

---
+++ index.js
@@ -0,0 +1,79 @@
+/* Login/Logout API
+ *
+ * All login/logout API calls are just different HTTP verbs
+ * operating on the /session resource. POST-ing to /session,
+ * given a correct username and password, will create a new
+ * session. DELETE-ing a /session effectively logs the user
+ * out, as it destroys that particular session object.
+ *
+ */
+
+var Server = require("./server"),
+    db = require("./db"),
+    app = new Server({ debug: true }),
+    crypto = require("crypto"),
+    sessions = {}; 
+
+/*
+ * Two different ways to create a token using this endpoint:
+ *
+ *   1. In POST body:
+ *
+ *      POST /login
+ *      {
+ *          "username": "myusername",
+ *          "password": "passw0rd"
+ *      }
+ *
+ * and
+ *
+ *   2. Create string "username:password", base64 encode that string, and
+ *      set that as the value of the Authorization header
+ *
+ *      POST /login
+ *      Authorization: dXNlcm5hbWU6cGFzc3dvcmQ=
+ *
+ *
+ * (I am assuming that this API is running over HTTPS, so I am
+ * not worried about sending credentials directly in the post body,
+ * or base64'd in the Authorization header)
+ */
+app.post("/session", function(args) {
+    var data = args.data,
+        headers = args.headers;
+
+    var username, password, response;
+
+    /* Login parameters were included in the POST body */
+    if (typeof data.username !== "undefined" &&
+            typeof data.password !== "undefined") {
+        username = data.username;
+        password = data.password;
+    } else if (typeof headers["authorization"] !== "undefined") {
+        var pair = new Buffer(headers["authorization"], 'base64').toString("utf8").split(":");
+        username = pair[0];
+        password = pair[1];
+    } else {
+        return app.error(401, "Unauthorized");
+    }
+
+    db.read({ username: username }, function(err, result) {
+        if (result.length < 1) {
+            response = app.error(401, "Unauthorized");
+            return;
+        }
+
+        var stored_pass = result[0].password;
+        if (password === stored_pass) {
+            var token = crypto.pseudoRandomBytes(32).toString('base64');
+            sessions[username] = token;
+            response = JSON.stringify({ token: token });
+        } else {
+            response = app.error(401, "Unauthorized");
+        }
+    });
+
+    return response;
+});
+
+app.serve();
