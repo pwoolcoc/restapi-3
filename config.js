2ed3cd8ea63cd4219a7a23888f8bc7a0209ec2fb
Paul Woolcock <paul@woolcock.us>
2015-08-28 11:59:34 -0400

    add create, update, delete

--- config.js
+++ config.js
@@ -1,23 +1,38 @@
 module.exports = {
+    URL: "/configuration",
     all: function(app) {
         var configs = app.configs
         return function(args) {
             var repsonse;
             var all_configs = configs.read("*", function(err, result) {
-                response = {
-                    status: 200,
-                    content: JSON.stringify({ "configurations": result }),
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                };
+                response = app.json_response({ "configurations": result });
             });
             return response;
         };
     },
     create: function(app) {
-        var configs = app.configs
+        var configs = app.configs,
+            self = this;
         return function(args) {
+            var response,
+                data = args.data;
+            configs.read({ "name": data.name }, function(err, result) {
+                if (result.length > 0) {
+                    response = app.error(400, "Bad Request");
+                } else {
+                    configs.create(data);
+                    var obj = JSON.stringify({ configuration: self.URL + "/" + data.name });
+                    response = {
+                        status: 201,
+                        content: obj,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Content-Length": obj.length
+                        }
+                    };
+                }
+            });
+            return response;
         };
     },
     get: function(app) {
@@ -43,11 +58,33 @@ module.exports = {
     update: function(app) {
         var configs = app.configs
         return function(args) {
+            var response,
+                captures = args.captures,
+                name = captures[0],
+                data = args.data;
+
+            configs.update({ "name": name }, data);
+            return {
+                status: 204,
+                content: ""
+            };
         };
     },
     delete: function(app) {
         var configs = app.configs
         return function(args) {
+            var response,
+                captures = args.captures,
+                name = captures[0];
+            var deleted = configs.delete({ "name": name });
+            if (deleted) {
+                return {
+                    status: 204,
+                    content: ""
+                };
+            } else {
+                return app.error(404, "Not Found");
+            }
         };
     }
 };
